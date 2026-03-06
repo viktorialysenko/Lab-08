9a83240f2ae928532b27036d9dc1e24fdba362de
“Viktoria <112983433+viktorialysenko@users.noreply.github.com>
2026-03-06 14:45:10 -0700

    Implemented Delete City

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -14,10 +14,12 @@ public class CustomList {
         cities.add(city);
     }
 
-    
-
     // Will be implemented later using TDD (leave blank for now)
     public boolean hasCity(City city) {
         return cities.contains(city);
+    }
+
+    public void deleteCity(City city) {
+        cities.remove(city);
     }
 }
